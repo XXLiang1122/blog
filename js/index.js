64066a22aa35935808352fb187d3889d79d40eb8
xiexueliang <792700864@qq.com>
2017-11-18 23:13:19 +0800

    update blog page

--- js/index.js
+++ js/index.js
@@ -1,31 +1,104 @@
-(function(){
+
+$(function() {
     //随机生成25个小方块
-    for (var i = 0; i < 25; i++) {
-        var l = Math.floor(Math.random() * 700),
-            t = Math.floor(Math.random() * 200),
-            r = Math.floor(Math.random() * 90);
+    (function(){
 
-        var $e = $('<div></div>').css({
-            'left': l + 'px',
-            'top': t + 'px',
-            'transform': 'rotate(' + r + 'deg)'
+        for (var i = 0; i < 25; i++) {
+            var l = Math.floor(Math.random() * 550),
+                t = Math.floor(Math.random() * 150),
+                r = Math.floor(Math.random() * 90);
+
+            var $e = $('<div></div>').css({
+                'left': l + 'px',
+                'top': t + 'px',
+                'transform': 'rotate(' + r + 'deg)'
+            });
+
+            $('.boxes').append($e);
+        }
+    })();
+
+    var judge = true;
+
+    //小方块拼凑动画
+    $('.left-list ul li').click(function () {
+
+        var count = 0,
+            $index = $(this).index();
+
+        $('.container>div').eq($index).show().siblings('div').hide();
+
+        if (judge) {
+            $('.boxes div').each(function(i, e) {
+
+                var newLeft = 280 + 180 * (i % 5),
+                    newTop = 30 + 140 * Math.floor(i / 5);
+
+                $(e).animate({
+                    left: newLeft + 'px',
+                    top: newTop + 'px',
+                    width: '180px',
+                    height: '140px',
+                    opacity: '0.8',
+                }, 800, function () {
+                    $(e).addClass('rotate-zero');
+                    count++;
+                    if (count > 24) {
+                        setTimeout(function () {
+                            $('.container').show();
+
+                        }, 300);
+
+                    }
+                });
+
+            });
+
+        }
+
+        judge = false;
+    });
+
+    //关闭动画
+    $('.container .close').click(function () {
+        $('.container').hide();
+        $('.boxes div').removeClass('rotate-zero');
+
+        $('.boxes div').each(function (i, e) {
+            var l = Math.floor(Math.random() * 700),
+                t = Math.floor(Math.random() * 200),
+                r = Math.floor(Math.random() * 90);
+
+            $(e).animate({
+                left: l + 'px',
+                top: t + 'px',
+                transform: 'rotate(' + r + 'deg)',
+                width: '40px',
+                height: '40px',
+                opacity: '0.2'
+            }, 800);
         });
 
-        $('.boxes').append($e);
-    }
-})();
+        judge = true;
+    });
 
-//小方块拼凑动画
-$('.left_list ul li').click(function () {
-    var $box = $('.boxes div');
 
-    for (var i = 0; i < 25; i++) {
-        var new_l = 300 + 120 * (i % 5),
-            new_t = 80 + 120 * Math.floor(i / 5);
+    //博客点击跳转iframe
+    $('.container .blog .blog-home ul li a').click(function(event) {
+        var $href = $(this).attr('href');
+        event.preventDefault();
 
-        $box[i].animate({
-            left: new_l + 'px',
-            top: new_t + 'px'
-        });
-    }
+        $('.container .blog .frame').attr('src', $href);
+
+        $('.container .blog .frame').show().siblings().hide();
+
+        $('.container .blog .return').show();
+    });
+
+    //返回上一页
+    $('.container .blog .return').click(function(event) {
+        $('.container .blog .frame').hide().siblings().show();
+        $(this).hide();
+    });
+
 });
