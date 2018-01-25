7676b7e0180639cff0ff24a11c8d53313b5cf58b
xiexueliang <xiexueliang@ihuayue.cn>
2018-01-25 15:22:16 +0800

    背景定位

--- html/js/enlargeImg.js
+++ html/js/enlargeImg.js
@@ -1,5 +1,5 @@
 if (window.screen.width < 1024) {
-	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	var scrollTop = 0;
 	var html = document.documentElement;
 	var body = document.getElementsByTagName('body')[0];
 	var dialog = document.getElementById('dialog');
@@ -7,12 +7,14 @@ if (window.screen.width < 1024) {
 	document.getElementsByClassName('content')[0].addEventListener('click', function (e) {
 		var e = e || window.event;
 	    var target = e.target || e.srcElement;
+	    scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		if (target.nodeName.toLowerCase() === 'img') {
 			var src = target.getAttribute('src');
 			dialog.style.display = 'flex';
 			img.setAttribute('src', src);
 			body.classList.add('noScroll');
 			html.classList.add('noScroll');
+			body.scrollTop = scrollTop;
 		}
 	});
 	dialog.addEventListener('click', function () {
