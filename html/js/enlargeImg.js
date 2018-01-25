bdf64329005623681f44d68d59ed15415966ea88
xiexueliang <xiexueliang@ihuayue.cn>
2018-01-25 11:47:04 +0800

    增加图片点击放大

---
+++ html/js/enlargeImg.js
@@ -0,0 +1,20 @@
+if (window.screen.width < 1024) {
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	var body = document.getElementsByTagName('body')[0];
+	var dialog = document.getElementById('dialog');
+	var img = document.getElementById('img');
+	document.getElementsByClassName('content')[0].addEventListener('click', function (e) {
+		var e = e || window.event;
+	    var target = e.target || e.srcElement;
+		if (target.nodeName.toLowerCase() === 'img') {
+			var src = target.getAttribute('src');
+			dialog.style.display = 'flex';
+			img.setAttribute('src', src);
+			body.classList.add('noScroll');
+		}
+	});
+	dialog.addEventListener('click', function () {
+		dialog.style.display = 'none';
+		body.classList.remove('noScroll');
+	});
+}
